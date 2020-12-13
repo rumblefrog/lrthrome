63ebf684762199ef6d8141f102d5c914538f7c1a
rumblefrog <contact@zhenyangli.me>
2020-12-13 06:54:57 -0500

    feat(server) - connection handling

--- server/src/lrthrome.rs
+++ server/src/lrthrome.rs
@@ -1,33 +1,149 @@
-use std::net::SocketAddr;
+use std::collections::HashMap;
+use std::net::{Shutdown, SocketAddr};
+use std::sync::Arc;
+use std::time::Instant;
+
+use bytes::BytesMut;
 
 use tokio::net::{TcpListener, TcpStream, ToSocketAddrs};
+use tokio::select;
+use tokio::sync::mpsc;
+use tokio::sync::watch::{channel, Sender};
+use tokio::sync::RwLock;
+use tokio::time::{sleep, Duration};
 
 use crate::cache::Cache;
 use crate::error::LrthromeResult;
+use crate::sources::Sources;
 
 pub struct Lrthrome {
     listener: TcpListener,
 
-    cache: Cache,
+    cache: Arc<RwLock<Cache>>,
 
-    streams: Vec<(TcpStream, SocketAddr)>,
+    clients: HashMap<SocketAddr, Client>,
+
+    temper_sources: Sources,
+
+    temper_interval: u64,
+}
+
+struct Client {
+    last_request: Instant,
+
+    shutdown: Sender<bool>,
+}
+
+enum Action {
+    Request(SocketAddr),
+    Shutdown(SocketAddr),
 }
 
 impl Lrthrome {
-    pub async fn new<A: ToSocketAddrs>(addr: A) -> LrthromeResult<Self> {
+    pub async fn new<A: ToSocketAddrs>(
+        addr: A,
+        temper_sources: Sources,
+        temper_interval: u64,
+    ) -> LrthromeResult<Self> {
         Ok(Self {
             listener: TcpListener::bind(addr).await?,
-            cache: Cache::new(),
-            streams: Vec::new(),
+            cache: Arc::new(RwLock::new(Cache::new())),
+            clients: HashMap::new(),
+            temper_sources,
+            temper_interval,
         })
     }
 
-    pub async fn up(&mut self) -> LrthromeResult<()> {
-        loop {
-            if let Ok(v) = self.listener.accept().await {
-                self.streams.push(v);
+    fn connection_heartbeats(&self) -> LrthromeResult<()> {
+        for (_, v) in &self.clients {
+            if v.last_request.elapsed() > Duration::from_secs(10) {
+                v.shutdown.send(true)?;
+            }
+        }
 
-                // Dispatch thread for processing
+        Ok(())
+    }
+
+    fn handle_connection(
+        &mut self,
+        addr: SocketAddr,
+        stream: TcpStream,
+        c_tx: mpsc::Sender<Action>,
+    ) {
+        let (tx, mut rx) = channel(false);
+
+        self.clients.insert(
+            addr,
+            Client {
+                last_request: Instant::now(),
+                shutdown: tx,
+            },
+        );
+
+        tokio::spawn(async move {
+            let mut buf = BytesMut::with_capacity(512);
+
+            loop {
+                select! {
+                    _ = rx.changed() => {
+                        stream.shutdown(Shutdown::Both).expect("Unable to shut down client stream");
+
+                        if c_tx.send(Action::Shutdown(addr)).await.is_err() {
+                            error!("Main receiver shutdown");
+                        }
+                    }
+                    Ok(ready) = stream.readable() => {
+                        if let Ok(n) = stream.try_read(&mut buf) {
+
+
+                            if c_tx.send(Action::Request(addr)).await.is_err() {
+                                error!("Main receiver shutdown");
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    pub async fn up(&mut self) -> LrthromeResult<()> {
+        self.connection_heartbeats()?;
+
+        let (temper_tx, mut temper_rx) = channel(Instant::now());
+
+        let temper_interval = self.temper_interval.clone();
+        tokio::spawn(async move {
+            temper_tx
+                .send(Instant::now())
+                .expect("Unable to send temper seq");
+
+            sleep(Duration::from_secs(60 * temper_interval)).await;
+        });
+
+        let (c_tx, mut c_rx) = mpsc::channel(64);
+
+        loop {
+            select! {
+                Ok(v) = temper_rx.changed() => {
+                    let mut cache = self.cache.write().await;
+
+                    cache.temper(&self.temper_sources).await?;
+                }
+                Some(action) = c_rx.recv() => {
+                    match action {
+                        Action::Request(addr) => {
+                            if let Some(v) = self.clients.get_mut(&addr) {
+                                v.last_request = Instant::now();
+                            }
+                        },
+                        Action::Shutdown(addr) => {
+                            self.clients.remove(&addr);
+                        }
+                    }
+                }
+                Ok((stream, addr)) = self.listener.accept() => {
+                    self.handle_connection(addr, stream, c_tx.clone());
+                }
             }
         }
     }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,22 +1,38 @@
+#[macro_use]
+extern crate log;
+
 use std::env::var;
 
-mod error;
-mod config;
-mod sources;
 mod cache;
+mod config;
+mod error;
 mod lrthrome;
+mod sources;
 
 use config::Config;
 use lrthrome::Lrthrome;
+use sources::{Remote, Sources};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config_loc = var("LRTHROME_CONFIG")
-        .unwrap_or("config.toml".into());
+    env_logger::init();
+
+    let config_loc = var("LRTHROME_CONFIG").unwrap_or("config.toml".into());
 
     let config: Config = toml::from_slice(&std::fs::read(config_loc)?)?;
 
-    let lrthrome = Lrthrome::new(config.general.bind_address).await?;
+    let mut sources = Sources::new();
+
+    sources.register(Box::new(Remote::new(config.sources.remotes)));
+
+    let mut lrthrome = Lrthrome::new(
+        config.general.bind_address,
+        sources,
+        config.general.temper_interval,
+    )
+    .await?;
+
+    lrthrome.up().await?;
 
     Ok(())
 }

ee180a9d4364ed171d31de98574143fad7ade884
rumblefrog <contact@zhenyangli.me>
2020-12-13 06:54:39 -0500

    feat(server) - sources

--- server/src/sources/mod.rs
+++ server/src/sources/mod.rs
@@ -1,3 +1,36 @@
-pub trait Source {
+use async_trait::async_trait;
 
+use cidr::Ipv4Cidr;
+
+use crate::error::LrthromeResult;
+
+mod remote;
+
+pub use remote::Remote;
+
+#[async_trait]
+pub trait Fetcher {
+    fn has_update(&self) -> bool;
+
+    async fn iterate_cidr(&self) -> LrthromeResult<Box<dyn Iterator<Item = Ipv4Cidr>>>;
+}
+
+pub struct Sources {
+    sources: Vec<Box<dyn Fetcher>>,
+}
+
+impl Sources {
+    pub fn new() -> Self {
+        Self {
+            sources: Vec::new(),
+        }
+    }
+
+    pub fn register(&mut self, source: Box<dyn Fetcher>) {
+        self.sources.push(source);
+    }
+
+    pub fn sources(&self) -> &Vec<Box<dyn Fetcher>> {
+        &self.sources
+    }
 }
---
+++ server/src/sources/remote.rs
@@ -0,0 +1,48 @@
+use std::str::FromStr;
+
+use async_trait::async_trait;
+
+use reqwest::Client;
+
+use cidr::Ipv4Cidr;
+
+use crate::error::LrthromeResult;
+
+use super::Fetcher;
+
+pub struct Remote {
+    endpoints: Vec<String>,
+}
+
+impl Remote {
+    pub fn new(endpoints: Vec<String>) -> Self {
+        Self { endpoints }
+    }
+}
+
+#[async_trait]
+impl Fetcher for Remote {
+    // It is uncertain until the file is fetched again
+    // Not all endpoints has E-tag to verify
+    fn has_update(&self) -> bool {
+        true
+    }
+
+    async fn iterate_cidr(&self) -> LrthromeResult<Box<dyn Iterator<Item = Ipv4Cidr>>> {
+        let client = Client::new();
+
+        let mut cidrs = Vec::new();
+
+        for endpoint in &self.endpoints {
+            let resp = client.get(endpoint).send().await?.text().await?;
+
+            let mut lines = resp.lines();
+
+            for line in lines.next() {
+                cidrs.push(Ipv4Cidr::from_str(line).unwrap());
+            }
+        }
+
+        Ok(Box::new(cidrs.into_iter()))
+    }
+}
